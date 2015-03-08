703372a10286df2b1e1b2a4c448ff9c50f69823e
mshocklee <mshocklee@users.noreply.github.com>
2015-03-08 18:14:11 -0500

    Plot4 Completion

---
+++ plot4.R
@@ -0,0 +1,20 @@
+dat <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?")
+dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
+dat <- dat[dat$Date == "2007-02-01" | dat$Date == "2007-02-02",]
+dat$Time <- strftime(strptime(dat$Time, "%H:%M:%S"), "%H:%M:%S")
+
+timeFrame <- strptime(paste(dat$Date, dat$Time, sep=" "), format="%Y-%m-%d %H:%M:%S")
+graphHeight <- max(c(max(dat$Sub_metering_1), max(dat$Sub_metering_2), max(dat$Sub_metering_3)))
+
+png(filename = "plot4.png", width=480, height=480)
+par(mfrow=c(2,2), mar=c(4,4,2,1))
+plot(timeFrame, dat$Global_active_power, xlab="", ylab="Global Active Power", type="l")
+plot(timeFrame, dat$Voltage, xlab="datetime", ylab="Voltage", type="l")
+plot(timeFrame, dat$Sub_metering_1, xlab="", ylab="Energy sub metering", type="l")
+par(new=T)
+plot(timeFrame, dat$Sub_metering_2, col="red", type="l", xlab="", ylab="", ylim=c(0,graphHeight))
+par(new=T)
+plot(timeFrame, dat$Sub_metering_3, col="blue", type="l", xlab="", ylab="", ylim=c(0,graphHeight))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue"), bty="n")
+plot(timeFrame, dat$Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type="l")
+dev.off()
