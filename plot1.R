2ac74891eb60d7ee5083bd46393bf64a9f2e28d6
mshocklee <mshocklee@users.noreply.github.com>
2015-03-08 17:56:58 -0500

    Plot3 Completion

--- plot1.R
+++ plot1.R
@@ -2,6 +2,7 @@ dat <- read.table("household_power_consumption.txt", header=T, sep=";", na.strin
 dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
 dat <- dat[dat$Date == "2007-02-01" | dat$Date == "2007-02-02",]
 dat$Time <- strftime(strptime(dat$Time, "%H:%M:%S"), "%H:%M:%S")
+
 png(filename = "plot1.png", width=480, height=480)
 hist(dat$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylim=c(0,1200))
 dev.off()
--- plot2.R
+++ plot2.R
@@ -2,6 +2,9 @@ dat <- read.table("household_power_consumption.txt", header=T, sep=";", na.strin
 dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
 dat <- dat[dat$Date == "2007-02-01" | dat$Date == "2007-02-02",]
 dat$Time <- strftime(strptime(dat$Time, "%H:%M:%S"), "%H:%M:%S")
+
+timeFrame <- strptime(paste(dat$Date, dat$Time, sep=" "), format="%Y-%m-%d %H:%M:%S")
+
 png(filename = "plot2.png", width=480, height=480)
-plot(strptime(paste(dat$Date, dat$Time, sep=" "), format="%Y-%m-%d %H:%M:%S"), dat$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l")
+plot(timeFrame, dat$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l")
 dev.off()
