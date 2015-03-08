11bd9bbe0fb9ad27369197f342aa1f6a13ccec8b
mshocklee <mshocklee@users.noreply.github.com>
2015-03-08 17:57:41 -0500

    Plot3 Actual Completion

--- plot3.R
+++ plot3.R
@@ -4,7 +4,13 @@ dat <- dat[dat$Date == "2007-02-01" | dat$Date == "2007-02-02",]
 dat$Time <- strftime(strptime(dat$Time, "%H:%M:%S"), "%H:%M:%S")
 
 timeFrame <- strptime(paste(dat$Date, dat$Time, sep=" "), format="%Y-%m-%d %H:%M:%S")
+graphHeight <- max(c(max(dat$Sub_metering_1), max(dat$Sub_metering_2), max(dat$Sub_metering_3)))
 
-png(filename = "plot2.png", width=480, height=480)
-plot(timeFrame, dat$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l")
+png(filename = "plot3.png", width=480, height=480)
+plot(timeFrame, dat$Sub_metering_1, xlab="", ylab="Energy sub metering", type="l")
+par(new=T)
+plot(timeFrame, dat$Sub_metering_2, col="red", type="l", xlab="", ylab="", ylim=c(0,graphHeight))
+par(new=T)
+plot(timeFrame, dat$Sub_metering_3, col="blue", type="l", xlab="", ylab="", ylim=c(0,graphHeight))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue"))
 dev.off()
